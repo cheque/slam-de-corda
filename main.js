4d625062db93c3be2b3e5cb803207412cdff3ba8
cheque <cheque.soto@gmail.com>
2020-10-25 20:01:13 -0300

    feat: consult data-base and show images

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-// var grid_canvas = $("#my_canvas");
 var canvas = document.getElementById("thecanvas");
 var ctx = canvas.getContext("2d");
 var viewWidth = window.innerWidth;
@@ -17,38 +16,98 @@ var back = new Image();
 var varal = new Image();
 var team = new Image();
 var ieb = new Image();
+var img = new Image();
+img.src = 'img/blank.png';
 
 var up = 0;
 var down = 1;
 
-function init() {
+var cordeis = document.getElementById('catalogue');
+
+function init(content) {
   back.src = 'img/fundo_cidade_preto_fundocordel.png';
   varal.src = 'img/slam_de_corda.png';
   team.src = 'img/logo_sesamo1branco_fundo_transp.png';
   ieb.src = 'img/IEBsite122.png';
-  window.requestAnimationFrame(draw);
+  window.requestAnimationFrame(draw,content);
 }
 
-function draw() {
+function draw(content) {
   ctx.globalCompositeOperation = 'destination-source';
   ctx.clearRect(0, 0, w, h); // clear canvas
   // ctx.drawImage(team );
   ctx.drawImage(back,0,0,w,h);
 
-  ctx.save();
-  if (Math.random() < 0.03 && up < 0.15) {
-    up = up + 0.01;
-  } else if (Math.random() < 0.03 && up > -0.15){
-    up = up - 0.01;
-  }
-  ctx.transform(1, 0, up, 1, 0, 0)
-  ctx.drawImage(varal,-10,50,w+20,(w+20)*varal.height/varal.width);
-  ctx.restore();
-
   ctx.drawImage(ieb,50,h-(ieb.height + 50));
   ctx.drawImage(team,w-(team.width + 50),h-(team.height + 40));
 
-  window.requestAnimationFrame(draw);
+  if (content){
+    ctx.drawImage(img,350,150,500,500*img.height/img.width);
+    // console.log(img.src);
+  }
+
+  ctx.save();
+  if (Math.random() < 0.03 && up > -0.15){
+    up = up - 0.01;
+  } else if (Math.random() < 0.03 && up < 0.15) {
+    up = up + 0.01;
+  }
+  ctx.transform(1, 0, up, 1, 0, 0)
+  ctx.drawImage(varal,-10,20,w+20,(w+20)*varal.height/varal.width);
+  ctx.restore();
+
+  window.requestAnimationFrame(draw,content);
 }
+
+window.onresize = function(){
+  viewWidth = window.innerWidth;
+  viewHeight = window.innerHeight;
+  canvas.setAttribute("width", viewWidth);
+  canvas.setAttribute("height", viewHeight);
+  w = canvas.width;
+  h = canvas.height;
+}
+
+function selector(value){
+  document.getElementById("menu").style.visibility = 'hidden';
+  switch (value) {
+    case 1:
+      document.getElementById("content1").style.visibility = 'visible';
+      break;
+    default:
+      document.getElementById("menu").style.visibility = 'visible';
+  }
+}
+
+function show(){
+  var id = document.getElementById("catalogue").value;
+  var pages = Cordeis[id].Folhas;
+  document.getElementById("titulo").innerHTML = "";
+  document.getElementById("titulo").innerHTML = Cordeis[id].Titulo;
+  document.getElementById("autor").innerHTML = "";
+  document.getElementById("autor").innerHTML = Cordeis[id].Autor;
+  document.getElementById("data").innerHTML = "";
+  document.getElementById("data").innerHTML = Cordeis[id].Data;
+  document.getElementById("localidade").innerHTML = "";
+  document.getElementById("localidade").innerHTML = Cordeis[id].Localidade;
+  document.getElementById("page").max = Cordeis[id].Folhas;
+
+  var p = document.getElementById("page").value;
+
+  img.src = 'cordeis/'+ Cordeis[id].Codigo + '-' + p + '.jpg';
+  init(true);
+
+}
+
+
+window.onload = function() {
+  var option;
+  for (var i = 0; i < Cordeis.length; i++) {
+      option = document.createElement('option');
+      option.text = Cordeis[i].Titulo;
+      option.value = i;
+      cordeis.add(option);
+  }
+};
 
 init();
